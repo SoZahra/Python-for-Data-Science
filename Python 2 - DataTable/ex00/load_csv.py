e421f4a76b482e2839ced5bc3f98892d97d32fe3
Fatima Zahra <zayani.fatima.z@gmail.com>
2025-10-11 17:22:53 +0200

    ajout de l'exo 00

---
+++ Python 2 - DataTable/ex00/load_csv.py
@@ -0,0 +1,26 @@
+import pandas as pd
+#(You have to adapt the type of return according to your library)
+
+def load(path: str):
+	"""
+	Ecrire kes dimensions d'un fichier data et return
+	NumPy = pour les tableaux numériques (images, matrices)
+	Pandas = pour les tableaux de donnees (personnes, statistiques, bases de données)
+	On va utiliser Pandas, c'est quoi avoir un excel en python
+	"""
+	assert isinstance(path, str), "Path must be an string"
+	assert path != "", "Path cannot be empty"
+
+	try:
+		data = pd.read_csv(path)
+
+		print(f"Loading dataset of dimensions{data.shape}")
+		return data.head(3)
+
+	except FileNotFoundError:
+		print("Error: file not found")
+		return None
+	except Exception as e:
+		print(f"Error: {e}")
+		return None
+
